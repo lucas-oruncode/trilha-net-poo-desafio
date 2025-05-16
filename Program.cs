4c4fe8c3ffa6d0a4195271b7f51771c49a81dc60
Lucas J <oruncode@gmail.com>
2025-05-15 22:20:23 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new("999-999-999", "14S", "nem sei", 16);
+Nokia nokia = new("888-888-888", "Q3", "meizin", 8);
+
+Console.WriteLine("========= IPHONE =========");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("iZap");
+
+Console.WriteLine("========= NOKIA =========");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Orkut");
